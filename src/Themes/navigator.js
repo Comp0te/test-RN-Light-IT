fa5389d669499fca3f1ec6466e0e56cde458ecb6
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-06-14 12:32:27 +0300

    Fix navigator styles

--- src/Themes/navigator.js
+++ src/Themes/navigator.js
@@ -1,27 +1,27 @@
-import {Dimensions, StyleSheet} from 'react-native';
-import {Color} from './colors'
+import { Dimensions } from 'react-native';
+import { Color } from './colors'
 
-export const headerTitleStyle = StyleSheet.create({
+export const headerTitleStyle = {
   fontWeight: '700',
   fontSize: 16,
   color: Color.MAIN,
   alignSelf: 'center',
-});
+};
 
-export const headerStyle = StyleSheet.create({
+export const headerStyle = {
   alignSelf: 'center',
   height: 49,
   width: '100%',
   backgroundColor: Color.WHITE,
   borderBottomWidth: 0.5,
   borderBottomColor: 'rgba(0, 0, 0, 0.25)',
-});
+};
 
-export const headerTitleContainerStyle = StyleSheet.create({
+export const headerTitleContainerStyle = {
   position: 'absolute',
   left: 30,
   right: 30,
   justifyContent: 'center',
   width: '100%',
   maxWidth: Dimensions.get('window').width - 60,
-});
+};
