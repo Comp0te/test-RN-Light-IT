ee03a3187189593642397343a93069981c48c563
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-06-17 17:46:54 +0300

    Add Drawer navigator

--- src/navigation/main.navigator.js
+++ src/navigation/main.navigator.js
@@ -1,17 +1,64 @@
 import React from 'react';
-import { createStackNavigator } from 'react-navigation';
+import { Dimensions } from 'react-native';
+import { createDrawerNavigator } from 'react-navigation';
+import { Color } from '../Themes/colors';
+import { Icon } from 'react-native-material-ui';
 
 import navService from '../services/nav.service';
 
 import ProductsScreen from '../screens/ProductsScreen';
 
-const mainNavigator = createStackNavigator({
+const mainNavigator = createDrawerNavigator({
   ProductsScreen: {
     screen: ProductsScreen,
     navigationOptions: navService.navigationOptions('Available products'),
   },
+  SavedProductsScreen: {
+    screen: ProductsScreen,
+    navigationOptions: navService.navigationOptions('Saved products'),
+  },
+  SettingsScreen: {
+    screen: ProductsScreen,
+    navigationOptions: navService.navigationOptions('Settings'),
+  },
 }, {
+  drawerWidth: Dimensions.get('window').width * 0.8,
   initialRouteName: 'ProductsScreen',
+  contentOptions: {
+    activeTintColor: Color.MAIN,
+    inactiveTintColor: Color.GREY,
+  },
+  defaultNavigationOptions: ({navigation}) => ({
+    drawerIcon: ({tintColor}) => {
+      const {routeName} = navigation.state;
+
+      if (routeName === 'ProductsScreen') {
+        return (
+          <Icon
+            name='laptop'
+            size={22}
+            color={tintColor ? tintColor : undefined}
+          />
+        );
+      } else if (routeName === 'SavedProductsScreen') {
+        return (
+          <Icon
+            name='shopping-cart'
+            size={22}
+            color={tintColor ? tintColor : undefined}
+          />
+        );
+      } else {
+        return (
+          <Icon
+            name='settings'
+            size={22}
+            color={tintColor ? tintColor : undefined}
+          />
+        );
+      }
+    },
+  }),
 });
 
 export default mainNavigator;
