0ae77b9c6a8517846c75dcb75e8c3011edca563a
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-06-17 14:52:21 +0300

    Add getProductByIdFromProps selector

--- src/redux/products/selectors.js
+++ src/redux/products/selectors.js
@@ -1,2 +1,16 @@
+import { createSelector } from 'reselect'
+
 export const getProductsEntities = state => state.products.entities;
 export const getProductsAllIds = state => state.products.allIds;
+
+export const getProductIdFromProps = (state, productItemProps) => productItemProps.productId;
+
+export const getProductByIdFromProps = createSelector(
+  [
+    getProductsEntities,
+    getProductIdFromProps,
+  ],
+  (productsEntities, productId) => {
+    return productsEntities?.[productId];
+  },
+);
