a6567916163ba5c0d9d1c2e18698ecd748ee7330
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-06-17 14:51:26 +0300

    Rework getAllProducts and getAllReviews saga

--- src/redux/requests/getAllProducts/saga.js
+++ src/redux/requests/getAllProducts/saga.js
@@ -2,20 +2,17 @@ import { put, call, takeEvery } from 'redux-saga/effects';
 import apiService from '../../../services/api.service';
 import { Actions, ActionTypes } from './AC';
 
-export function * getAllProductsRequestSaga() {
+export function* getAllProductsRequestSaga() {
   try {
     const response = yield call([apiService, apiService.getAllProducts]);
 
-    if (response.data.success) {
-      yield put(Actions.getAllProductsSuccess(response.data));
-    }
+    yield put(Actions.getAllProductsSuccess(response.data));
 
-    yield put(Actions.getAllProductsFail(response.data.message));
   } catch (error) {
     yield put(Actions.getAllProductsFail(error));
   }
 }
 
-export function * getAllProductsRequestRootSaga() {
+export function* getAllProductsRequestRootSaga() {
   yield takeEvery(ActionTypes.GET_ALL_PRODUCTS_REQUEST, getAllProductsRequestSaga);
 }
