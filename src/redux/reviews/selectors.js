d782cf214b885e07638f60e0eb004f8a1c0120e5
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-06-17 17:08:42 +0300

    Add new users and reviews selectors

--- src/redux/reviews/selectors.js
+++ src/redux/reviews/selectors.js
@@ -1,2 +1,34 @@
+import { createSelector } from 'reselect';
+import { getProductIdFromProps } from '../products/selectors'
+
 export const getReviewsEntities = state => state.reviews.entities;
 export const getReviewsAllIds = state => state.reviews.allIds;
+
+export const getReviewIdFromProps = (state, reviewItemProps) => reviewItemProps.reviewId;
+
+export const getReviewByIdFromProps = createSelector(
+  [
+    getReviewsEntities,
+    getReviewIdFromProps,
+  ],
+  (reviewsEntities, reviewId) => {
+    return reviewsEntities?.[reviewId];
+  },
+);
+
+export const getReviewsIdsByProductIdFromProps = createSelector(
+  [
+    getReviewsEntities,
+    getReviewsAllIds,
+    getProductIdFromProps,
+  ],
+  (reviewsEntities, reviewIds, productId) => {
+    return reviewIds.reduce((acc, cur) => {
+      if (reviewsEntities[cur].product === +productId) {
+        return acc.concat(cur);
+      }
+
+      return acc;
+    }, []);
+  },
+);
--- src/redux/users/selectors.js
+++ src/redux/users/selectors.js
@@ -1,2 +1,18 @@
+import { createSelector } from 'reselect';
+import { getReviewsEntities, getReviewIdFromProps } from '../reviews/selectors'
+
 export const getUsersEntities = state => state.users.entities;
 export const getUsersAllIds = state => state.users.allIds;
+
+export const getUserByReviewIdFromProps = createSelector(
+  [
+    getUsersEntities,
+    getReviewsEntities,
+    getReviewIdFromProps,
+  ],
+  (usersEntities, reviewsEntities, reviewId) => {
+    const userId = reviewsEntities?.[reviewId]?.userId;
+
+    return usersEntities?.[userId];
+  },
+);
