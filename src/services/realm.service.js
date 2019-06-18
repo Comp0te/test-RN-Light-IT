4f44f0d4ac4d802ca46a76e15af75a71483f5612
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-06-18 12:24:25 +0300

    Add realm.service

---
+++ src/services/realm.service.js
@@ -0,0 +1,45 @@
+import Realm from 'realm';
+import { Scheme, SchemaName } from '../realm/scheme'
+
+class RealmService {
+  _realm;
+  dbConfig = {
+    schema: Scheme,
+    schemaVersion: 1,
+  };
+  SchemaName = SchemaName;
+
+  async initialize() {
+    try {
+      this._realm = await Realm.open(this.dbConfig)
+    } catch (e) {
+      console.log('initialize error - ', e)
+    }
+  }
+
+  async read(schemaName) {
+    try {
+      return await this._realm.objects(schemaName)
+    } catch (e) {
+      console.log('read error - ', e)
+    }
+  }
+
+  async write(schemaName, entity) {
+    try {
+      return await this._realm.write(() => this._realm.create(schemaName, entity, true))
+    } catch (e) {
+      console.log('write error - ', e)
+    }
+  }
+
+  async delete(entityForDelete) {
+    try {
+      return await this._realm.delete(entityForDelete)
+    } catch (e) {
+      console.log('delete error - ', e)
+    }
+  }
+}
+
+export default new RealmService();
