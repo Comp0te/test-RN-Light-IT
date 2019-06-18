3603a45ed731d6cb018f46d68b0ffe4f86f1a18d
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-06-18 12:22:02 +0300

    Add primary key to realm Auth schema

--- src/realm/scheme/auth.schema.js
+++ src/realm/scheme/auth.schema.js
@@ -2,7 +2,9 @@ export const AUTH_SCHEMA_NAME = 'Auth';
 
 export const AuthSchema = {
     name: AUTH_SCHEMA_NAME,
+    primaryKey: 'id',
     properties: {
+        id: 'string',
         token:  'string',
         userId: 'string?',
         username: 'string?',
