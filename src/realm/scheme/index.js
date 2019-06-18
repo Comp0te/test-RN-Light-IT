77df5af6d587235d1740ea2699d14a8734b4ce6c
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-06-18 09:02:44 +0300

    Change realm scheme structure

---
+++ src/realm/scheme/index.js
@@ -0,0 +1,11 @@
+import { AuthSchema } from './auth.schema'
+import { ProductSchema } from './product.schema'
+import { ReviewSchema } from './review.schema'
+import { UserSchema } from './user.schema'
+
+export const Scheme = [
+  AuthSchema,
+  ProductSchema,
+  ReviewSchema,
+  UserSchema,
+];
