70f4b035806258d5d536320dea6d67616f0c1eb0
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-06-19 18:03:25 +0300

    Add realm settings.schema

--- src/realm/scheme/index.js
+++ src/realm/scheme/index.js
@@ -2,12 +2,14 @@ import { AuthSchema, AUTH_SCHEMA_NAME } from './auth.schema'
 import { ProductSchema, PRODUCT_SCHEMA_NAME } from './product.schema'
 import { ReviewSchema, REVIEW_SCHEMA_NAME } from './review.schema'
 import { UserSchema, USER_SCHEMA_NAME } from './user.schema'
+import { SettingsSchema, SETTINGS_SCHEMA_NAME } from './settings.schema'
 
 export const Scheme = [
   AuthSchema,
   ProductSchema,
   ReviewSchema,
   UserSchema,
+  SettingsSchema,
 ];
 
 export const SchemaName = {
@@ -15,4 +17,5 @@ export const SchemaName = {
   PRODUCT: PRODUCT_SCHEMA_NAME,
   REVIEW: REVIEW_SCHEMA_NAME,
   USER: USER_SCHEMA_NAME,
+  SETTINGS: SETTINGS_SCHEMA_NAME
 };
---
+++ src/realm/scheme/settings.schema.js
@@ -0,0 +1,11 @@
+export const SETTINGS_SCHEMA_NAME = 'Settings';
+
+export const SettingsSchema = {
+    name: SETTINGS_SCHEMA_NAME,
+    primaryKey: 'id',
+    properties: {
+        id: 'string',
+        isTouchIDAuth: 'bool?',
+        language: 'string?',
+    }
+};
