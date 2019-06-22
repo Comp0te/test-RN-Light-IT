e25a456ebcb2739b17d3a4e38eaa20718c8cf196
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-06-22 23:30:44 +0300

    Add i18n.service

---
+++ src/services/i18n.service.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class I18nService {
+  _i18n;
+
+  set i18n(value) {
+    this._i18n = value;
+  }
+
+  changeLanguage(language) {
+    if (this._i18n.isInitialized) {
+      this._i18n.changeLanguage(language)
+    }
+  }
+}
+
+export default new I18nService();
