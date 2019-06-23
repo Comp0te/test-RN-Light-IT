8f33beaeaffb0f2997f534f6b09b5c7903861a38
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-06-23 16:14:48 +0300

    Populate en and ru translation dictionary

--- src/translation/en.js
+++ src/translation/en.js
@@ -1,1 +1,36 @@
-export const en = {};
+import { ScreenRouteNames } from '../utils/constants';
+
+export const en = {
+  [ScreenRouteNames.LOGIN_SCREEN]: {
+    'User Name': 'User Name',
+    'Password': 'Password',
+    'Submit': 'Submit',
+    'Sign in with': 'Sign in with',
+    'Do not have an account': 'Do not have an account? ',
+    'Sign up': 'SING UP',
+  },
+  [ScreenRouteNames.REGISTRATION_SCREEN]: {
+    'User Name': 'User Name',
+    'Password': 'Password',
+    'Confirm Password': 'Confirm Password',
+    'Submit': 'Submit',
+  },
+  [ScreenRouteNames.SETTINGS_SCREEN]: {
+    'Application language': 'Application language',
+    'Touch ID': 'Touch ID authorization',
+    'Log out': 'Log out',
+  },
+  components: {
+    'No products': 'You have not saved any products.',
+  },
+  navigation: {
+    'Sign In': 'Sign In',
+    'Registration': 'Registration',
+    'Settings': 'Settings',
+    'Saved products': 'Saved products',
+    'Delete': 'Delete',
+    'Save': 'Save',
+    'Available products': 'Available products',
+    'Products': 'Products',
+  },
+};
--- src/translation/ru.js
+++ src/translation/ru.js
@@ -1,1 +1,37 @@
-export const ru = {};
+import { ScreenRouteNames } from '../utils/constants';
+
+export const ru = {
+  [ScreenRouteNames.LOGIN_SCREEN]: {
+    'User Name': 'Имя пользователя',
+    'Password': 'Пароль',
+    'Submit': 'Отправить',
+    'Sign in with': 'Войти с помощью',
+    'Do not have an account': 'Еще не регистрировались? ',
+    'Sign up': 'Зарегистрироваться'
+  },
+  [ScreenRouteNames.REGISTRATION_SCREEN]: {
+    'User Name': 'Имя пользователя',
+    'Password': 'Пароль',
+    'Confirm Password': 'Подтвердите пароль',
+    'Submit': 'Отправить',
+  },
+  [ScreenRouteNames.SETTINGS_SCREEN]: {
+    'Application language': 'Язык приложения',
+    'Touch ID': 'Touch ID авторизация',
+    'Log out': 'Выйти',
+    'title': 'Настройки',
+  },
+  components: {
+    'No products': 'Вы не сохранили никаких продуктов.',
+  },
+  navigation: {
+    'Sign In': 'Вход',
+    'Registration': 'Регистрация',
+    'Settings': 'Настройки',
+    'Saved products': 'Сохраненные продукты',
+    'Delete': 'Удалить',
+    'Save': 'Сохранить',
+    'Available products': 'Доступные продукты',
+    'Products': 'Продукты',
+  },
+};
