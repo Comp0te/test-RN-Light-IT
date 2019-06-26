0d3da4c6c6bd750ce6691de4141ec0b843e6a39a
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-06-26 15:41:53 +0300

    Configure react-native-camera on IOS and Android

--- android/app/src/main/java/com/compotetestrn/MainApplication.java
+++ android/app/src/main/java/com/compotetestrn/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.CallbackManager;
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.rnfs.RNFSPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
@@ -41,6 +42,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
             new RNFSPackage(),
             new RNGoogleSigninPackage(),
             new FBSDKPackage(mCallbackManager),
