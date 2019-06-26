7f0c1fb717a2e93e6f664a2b7f0d667e3470db48
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-06-26 12:11:37 +0300

    Configure react-native-fs on IOS and Android

--- android/app/src/main/java/com/compotetestrn/MainApplication.java
+++ android/app/src/main/java/com/compotetestrn/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.CallbackManager;
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
@@ -40,6 +41,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
             new RNGoogleSigninPackage(),
             new FBSDKPackage(mCallbackManager),
             new SplashScreenReactPackage(),
