ca37c9f1e3a51e30b2227ceb80f569547ed8f69b
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-07-01 14:48:57 +0300

    Add rnfs.service

---
+++ src/services/rnfs.service.js
@@ -0,0 +1,44 @@
+import * as RNFS from 'react-native-fs';
+import { Platform } from 'react-native';
+
+class RNFSService {
+  CAMERA_DIR_PATH = `${RNFS.DocumentDirectoryPath}/Camera`;
+
+  async createCameraDir() {
+    await RNFS.mkdir(this.CAMERA_DIR_PATH);
+  }
+
+  async readPhotoUrisFromCameraDir() {
+    const isCameraDirExist = await RNFS.exists(this.CAMERA_DIR_PATH);
+
+    if (!isCameraDirExist) {
+      await this.createCameraDir();
+    }
+
+    const RNFSData = await RNFS.readDir(this.CAMERA_DIR_PATH);
+    return RNFSData.map(
+      data => (Platform.OS === 'ios' ? data.path : `file://${data.path}`),
+    );
+  }
+
+  async deleteFile(path) {
+    try {
+      await RNFS.unlink(path);
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+
+  async movePhotoToCameraDir(path) {
+    const uriArray = path.split('/');
+    const destPath = `${this.CAMERA_DIR_PATH}/${uriArray[uriArray.length - 1]}`;
+
+    try {
+      await RNFS.moveFile(path, destPath);
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+}
+
+export default new RNFSService();
