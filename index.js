e277ac004abd74657f2dcc71acdba70d8d39d7b4
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-08-07 22:23:05 +0300

    Add bgMessaging.service

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-/**
- * @format
- */
-
 import { AppRegistry } from 'react-native';
 import App from './src/screens/App';
 import { name as appName } from './app.json';
 import './src/translation';
+import bgMessagingService from './src/services/bgMessaging.servise';
 
 AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => bgMessagingService);
